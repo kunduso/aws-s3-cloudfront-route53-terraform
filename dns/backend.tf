153c0f222f5946454929f376d4ce705a080e6a80
sourav kundu <kunduso@amazon.com>
2025-12-18 23:44:06 -0600

    fix for #4

---
+++ dns/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "kunduso-terraform-remote-bucket"
+    encrypt      = true
+    key          = "tf/aws-s3-cloudfront-route53-terraform/dns/terraform.tfstate"
+    region       = "us-east-2"
+    use_lockfile = true
+  }
+}
