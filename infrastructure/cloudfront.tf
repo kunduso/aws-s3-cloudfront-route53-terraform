0eb40f9e774008a5b65224f779e756f25f008b49
sourav kundu <kunduso@amazon.com>
2025-12-26 06:32:29 -0600

    enable minimum TLS 1.2. Fix for #11

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -36,8 +36,9 @@ resource "aws_cloudfront_distribution" "website" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate_validation.website.certificate_arn
-    ssl_support_method  = "sni-only"
+    acm_certificate_arn      = aws_acm_certificate_validation.website.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
