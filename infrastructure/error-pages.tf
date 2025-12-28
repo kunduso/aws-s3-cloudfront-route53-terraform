d935b3e63044f974299bb8fae5a28bbdb645292a
sourav kundu <kunduso@amazon.com>
2025-12-27 21:11:59 -0600

    add error page deployment for #21

---
+++ infrastructure/error-pages.tf
@@ -0,0 +1,17 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "error_404" {
+  bucket       = aws_s3_bucket.cloudfront_ops.id
+  key          = "error-pages/404.html"
+  source       = "${path.module}/../error-pages/404.html"
+  content_type = "text/html"
+  etag         = filemd5("${path.module}/../error-pages/404.html")
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "error_500" {
+  bucket       = aws_s3_bucket.cloudfront_ops.id
+  key          = "error-pages/500.html"
+  source       = "${path.module}/../error-pages/500.html"
+  content_type = "text/html"
+  etag         = filemd5("${path.module}/../error-pages/500.html")
+}
