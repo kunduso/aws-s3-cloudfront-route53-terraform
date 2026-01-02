2bed3360db6afc887f3364e980ac8a18bd96ae42
sourav kundu <kunduso@amazon.com>
2026-01-02 04:56:15 -0600

    moved error pages and updated path

--- infrastructure/error-pages.tf
+++ infrastructure/error-pages.tf
@@ -2,16 +2,16 @@
 resource "aws_s3_object" "error_404" {
   bucket       = aws_s3_bucket.cloudfront_ops.id
   key          = "error-pages/404.html"
-  source       = "${path.module}/../error-pages/404.html"
+  source       = "${path.module}/error-pages/404.html"
   content_type = "text/html"
-  etag         = filemd5("${path.module}/../error-pages/404.html")
+  etag         = filemd5("${path.module}/error-pages/404.html")
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "error_500" {
   bucket       = aws_s3_bucket.cloudfront_ops.id
   key          = "error-pages/500.html"
-  source       = "${path.module}/../error-pages/500.html"
+  source       = "${path.module}/error-pages/500.html"
   content_type = "text/html"
-  etag         = filemd5("${path.module}/../error-pages/500.html")
+  etag         = filemd5("${path.module}/error-pages/500.html")
 }
