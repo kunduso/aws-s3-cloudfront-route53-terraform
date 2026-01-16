7b9c098010ba1674c0278d70eb6a5cf6b0bb0227
sourav kundu <kunduso@amazon.com>
2026-01-15 19:30:16 -0600

    Fix CKV_AWS_145: Add KMS encryption to CloudFront ops bucket

--- infrastructure/cloudfront-logging.tf
+++ infrastructure/cloudfront-logging.tf
@@ -50,6 +50,18 @@ resource "aws_s3_bucket_versioning" "cloudfront_ops" {
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_server_side_encryption_configuration
+resource "aws_s3_bucket_server_side_encryption_configuration" "cloudfront_ops" {
+  bucket = aws_s3_bucket.cloudfront_ops.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_lifecycle_configuration
 resource "aws_s3_bucket_lifecycle_configuration" "cloudfront_ops" {
   bucket = aws_s3_bucket.cloudfront_ops.id
