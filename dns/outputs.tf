1cba63f5bceeabc4ce48991f91e69922790832bf
Sourav Kundu <skundu.dev@gmail.com>
2026-02-27 22:26:06 -0600

    add output for dnssec records. Fix for #45

--- dns/outputs.tf
+++ dns/outputs.tf
@@ -7,3 +7,14 @@ output "hosted_zone_id" {
   description = "Route53 hosted zone ID"
   value       = aws_route53_zone.main.zone_id
 }
+
+# Output DS record
+output "ds_record" {
+  description = "Formatted DS record for registrar"
+  value = format("%s %s %s %s",
+    aws_route53_key_signing_key.main.key_tag,
+    aws_route53_key_signing_key.main.signing_algorithm_mnemonic,
+    aws_route53_key_signing_key.main.digest_algorithm_mnemonic,
+    aws_route53_key_signing_key.main.digest_value
+  )
+}
